d07f6f88e86caa4e7dde9d636036849606beb5fc
German Canela <germein1495@hotmail.com>
2021-08-07 20:23:28 -0500

    Top 10

---
+++ Figuras-Geometricas/salarios.js
@@ -0,0 +1,101 @@
+const colombia =[];
+
+colombia.push({
+    name: "Juanita",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Natalia",
+    salary: 1200,
+});
+
+colombia.push({
+    name: "Geman",
+    salary: 5000,
+});
+
+colombia.push({
+    name: "Julieta",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Pedro",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Polo",
+    salary: 1100,
+});
+
+colombia.push({
+    name: "Argelia",
+    salary: 4000,
+});
+
+colombia.push({
+    name: "Paco",
+    salary: 3000,
+});
+
+colombia.push({
+    name: "Perri",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Barberra",
+    salary: 2000,
+});
+
+colombia.push({
+    name: "Rueda",
+    salary: 1000,
+});
+
+colombia.push({
+    name: "Ruth",
+    salary: 1200,
+});
+
+colombia.push({
+    name: "Ale",
+    salary: 5000,
+});
+
+colombia.push({
+    name: "Meli",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Arturo",
+    salary: 1500,
+});
+
+colombia.push({
+    name: "Luz",
+    salary: 1100,
+});
+
+colombia.push({
+    name: "Jose",
+    salary: 4000,
+});
+
+colombia.push({
+    name: "Alberto",
+    salary: 3000,
+});
+
+colombia.push({
+    name: "Jorge",
+    salary: 1800,
+});
+
+colombia.push({
+    name: "Miguel",
+    salary: 2000,
+});
